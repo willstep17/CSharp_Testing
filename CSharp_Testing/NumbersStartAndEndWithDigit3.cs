f759be096bb0ccff1aa6cce18bfbebe37473512d
William Stephens <william.a.stephens17@gmail.com>
2020-09-15 15:03:13 -0500

    First commit

---
+++ CSharp_Testing/NumbersStartAndEndWithDigit3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CSharp_Testing
+{
+    public class NumbersStartAndEndWithDigit3
+    {
+        public NumbersStartAndEndWithDigit3()
+        {
+        }
+
+        public static void Run()
+        {
+            int count = 3;
+            int n = 1000;
+
+            while(count < 100)
+            {
+                if (n % 10 == 3)
+                {
+                    int _n = n;
+                    int remain = 0;
+
+                    while (n > 0)
+                    {
+                        remain = _n % 10;
+                        _n = _n / 10;
+                    }
+
+                    if (remain == 3)
+                    {
+                        count++;
+                        Console.WriteLine($"{count}) {n}");
+                    }
+                }
+                n++;
+            }
+
+        }
+    }
+}
---
+++ CSharp_Testing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumbersStartAndEndWithDigit3.Run();
+        }
+    }
+}
